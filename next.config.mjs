f7a43c2e52b59962f40b700f4ae53bab9421e83d
monzila-akter <monzila01727@gmail.com>
2025-01-24 20:12:34 +0600

    doing change in env file

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,5 @@
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    env: {
-        KINDE_ISSUER_URL: "https://monzila.kinde.com",
-        KINDE_SITE_URL: "https://blog-viewers.vercel.app",
-        KINDE_POST_LOGOUT_REDIRECT_URL: "https://blog-viewers.vercel.app",
-        KINDE_POST_LOGIN_REDIRECT_URL: "https://blog-viewers.vercel.app",
-        KINDE_CLIENT_ID: "6a691ae49dce44389425d8de73d8a160",
-        KINDE_CLIENT_SECRET: "6a691ae49dce44389425d8de73d8a160",
-    }
-};
+const nextConfig = {};
 
 export default nextConfig;
--- src/app/AuthPriovider.js
+++ src/app/AuthPriovider.js
@@ -5,7 +5,7 @@ export const AuthProvider = ({children}) => {
     return <KindeProvider>
         {children}
     </KindeProvider>;
-    // authentication done
+    // authentication done completely
 };
 
 
