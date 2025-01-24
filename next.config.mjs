264668978f97b6a8d52f1779b966e9b1a2a2a5db
monzila-akter <monzila01727@gmail.com>
2025-01-24 19:37:06 +0600

    added vercel url for deploy

--- next.config.mjs
+++ next.config.mjs
@@ -2,9 +2,9 @@
 const nextConfig = {
     env: {
         KINDE_ISSUER_URL: "https://monzila.kinde.com",
-        KINDE_SITE_URL: "http://localhost:3000",
-        KINDE_POST_LOGOUT_REDIRECT_URL: "http://localhost:3000",
-        KINDE_POST_LOGIN_REDIRECT_URL: "http://localhost:3000",
+        KINDE_SITE_URL: "https://blog-viewers.vercel.app",
+        KINDE_POST_LOGOUT_REDIRECT_URL: "https://blog-viewers.vercel.app",
+        KINDE_POST_LOGIN_REDIRECT_URL: "https://blog-viewers.vercel.app",
         KINDE_CLIENT_ID: "6a691ae49dce44389425d8de73d8a160",
         KINDE_CLIENT_SECRET: "6a691ae49dce44389425d8de73d8a160",
     }
